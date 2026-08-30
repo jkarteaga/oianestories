78abea2d0f9fa71ad8e4bc2a9c2ef83866d6c4be
Jon Koldo Arteaga <jkarteaga@gmail.com>
2026-08-30 11:02:31 +0200

    Add embedsPlugin import to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { formsPlugin } from "@emdash-cms/plugin-forms";
 import webhookNotifier from "@emdash-cms/plugin-webhook-notifier";
 import { defineConfig, fontProviders } from "astro/config";
 import emdash from "emdash/astro";
+import { embedsPlugin } from '@emdash-cms/plugin-embeds';
 
 export default defineConfig({
 	output: "server",
